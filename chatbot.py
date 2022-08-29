057484c17b25b8cdd070799fdde03b7468bfc24c
Ederson Torresini <boidacarapreta@gmail.com>
2022-08-29 17:31:48 -0300

    Testar código de exemplo

---
+++ chatbot.py
@@ -0,0 +1,25 @@
+from os import getenv
+from dotenv import load_dotenv
+import discord
+load_dotenv()
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!hello'):
+            await message.reply('Hello!', mention_author=True)
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(getenv("DISCORD_TOKEN"))
