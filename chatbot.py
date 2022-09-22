2cc3d7098d80166ee5894e1cef04687f76f408c5
Ederson Torresini <boidacarapreta@gmail.com>
2022-09-21 21:12:56 -0300

    Reduzir operações de manipulação de dicionários
    Futuramente, reduzir as consultas em banco de dados não relacional.

--- chatbot.py
+++ chatbot.py
@@ -49,59 +49,57 @@ async def on_ready():
 
 @bot.event
 async def on_message(msg):
-    # Verificar se a mensagem não tem o próprio bot como autor.
-    if msg.author.id == bot.application_id:
-        return
+    # Armazenar o autor da mensagem
+    autor = msg.author.id
 
+    # Verificar se a mensagem não tem o próprio bot como autor.
+    if autor == bot.application_id:
+        return
+    #
     # Verificar se o jogador ainda não começou a partida,
     # o que significa que precisa colocá-lo no estado zero (0).
-    if msg.author.id not in partidas:
-        partidas[msg.author.id] = 0
-
+    if autor not in partidas:
+        partidas[autor] = 0
+    #
     # Em ordem de operação:
-    # 0) Obter o ID do jogador:
-    #    msg.author.id
-    # 1) Obter o estado atual do jogador:
-    #    partidas[msg.author.id]
-    # 2) Obter a definição completa desse estado:
-    #    estados[partidas[msg.author.id]]
+    # 0) Obter o estado atual do jogador:
+    #    partidas[autor]
+    # 1) Obter a definição completa desse estado:
+    #    estados[partidas[autor]]
+    estado_do_jogador = estados[partidas[autor]]
+    #
     # 3) Filtrar desse estado apenas a lista de próximos estados:
-    #    estados[partidas[msg.author.id]]['proximos_estados']
+    #    estados_do_jogador['proximos_estados']
     # 4) Filtrar dessa lista as chaves (frases) quem levam a próximos estados:
-    #    estados[partidas[msg.author.id]]['proximos_estados'].keys()
+    #    estado_de_jogador['proximos_estados'].keys()
     # 5) Verificar se a frase do usuário está na lista de chaves (frases) do estado:
-    if msg.content in estados[partidas[msg.author.id]]['proximos_estados'].keys():
+    if msg.content in estado_do_jogador['proximos_estados'].keys():
         #
-        # 6) Atualizar o estado do jogador, fazendo-o avançar no jogo:
-        partidas[msg.author.id] = estados[partidas[msg.author.id]]['proximos_estados'][msg.content]
+        # Atualizar o estado do jogador,
+        # e para isso é usado o conteúdo da mensagem como chave do dicionário:
+        partidas[autor] = estado_do_jogador['proximos_estados'][msg.content]
         #
-        # 7) Enviar para o jogador a mensagem do estado (já atualizado)
+        # A definição completa do estado do jogador,
+        # por consequência, também é atualizada
+        estado_do_jogador = estados[partidas[autor]]
+        #
+        # Enviar para o jogador a mensagem do estado (já atualizado)
         #
         # Em ordem de operação:
-        # 7.0) Obter o ID do jogador:
-        #    msg.author.id
-        # 7.1) Obter o estado atual do jogador:
-        #    partidas[msg.author.id]
-        # 7.2) Obter a definição completa desse estado:
-        #    estados[partidas[msg.author.id]]
-        # 7.3) Filtrar desse estado apenas a lista de frases:
-        #    estados[partidas[msg.author.id]]['frases']
-        # 7.4) Sortear uma frase dessa lista:
-        #   choice(estados[partidas[msg.author.id]]['frases'])
-        await msg.channel.send(choice(estados[partidas[msg.author.id]]['frases']))
+        # 0) Filtrar do estado do jogador apenas a lista de frases:
+        #    estado_do_jogador['frases']
+        # 1) Sortear uma frase dessa lista:
+        #   choice(estado_do_jogador['frases'])
+        await msg.channel.send(choice(estado_do_jogador['frases']))
     #
-    # Caso contrário, avisar que a mensagem não avança no jogo
+    # Caso contrário, avisar que a mensagem não avança no jogo.
+    # Se o jogador ainda estiver no estado, ajudar com uma dica:
+    elif partidas[autor] == 0:
+        await msg.channel.send(choice(estado_do_jogador['frases']))
     else:
         #
-        # Se o jogador está no primeiro estado...
-        if partidas[msg.author.id] == 0:
-            #
-            # ...ajudar com uma dica:
-            await msg.channel.send(choice(estados[partidas[msg.author.id]]['frases']))
-        else:
-            #
-            # Nos estados seguintes, a resposta padrão de HAL:
-            await msg.channel.send('I\'m sorry Dave, I\'m afraid I can\'t do that.')
+        #  Nos estados seguintes, a resposta padrão de HAL:
+        await msg.channel.send('I\'m sorry Dave, I\'m afraid I can\'t do that.')
 
 
 bot.run(getenv('DISCORD_TOKEN'))
